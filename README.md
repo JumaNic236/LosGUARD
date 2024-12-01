149562b24fdd4eb9acebcfa3c078403222d6f6bd
JumaNic236 <jumaniccc@gmail.com>
2024-12-01 20:05:42 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+🌐 Hızlı yazılım, güvenilir sonuçlar
+∙ https://www.markinsoftware.com.tr
+
+∙ info@markinsoftware.com.tr
+∙ Markin Software
---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "(MTMxMjExNzc2ODE5NTQwODAxMg.GrfnLV._kDhokMDXY-rOamYRx-ppH_1oTQS1GEYPIHH4s)", 
+    prefix: "!",
+    botdavet: "(https://discord.com/oauth2/authorize?client_id=1312117768195408012&permissions=8&integration_type=0&scope=bot)",
+    desteksunucusu: "(https://discord.gg/RmTSeRbE)",
+    website: "(www.losemires.com)",
+    politika: "(LosEmires)",
+    sunucuid: "(1287824489911943180)",    
+    destekçi: "(JumaNic)"
+}
+
